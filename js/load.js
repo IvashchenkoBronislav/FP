82e03ae1310493a897fdbe7fdde8a090087a2361
IvashchenkoBronislav <68849801+IvashchenkoBronislav@users.noreply.github.com>
2021-03-12 14:11:47 +0200

    fix reg

--- js/load.js
+++ js/load.js
@@ -3,4 +3,4 @@ let loader = document.querySelector(".load")
 
 setTimeout(()=>{
     loader.classList = "load_off"
-}, 1000)
+}, 2000)
--- js/modal.js
+++ js/modal.js
@@ -27,7 +27,7 @@ let modail_off_on = () =>{
         modailRegist.style = "display:none";
     }
     console.log(onReg,onLogIn )
-}
+};
 
 let reg_on = ()=>{
     onReg === true;
@@ -35,7 +35,7 @@ let reg_on = ()=>{
 
     onLogIn = false;
     modailLogIn.style = "display:none";
-}
+};
 
 let reg_off = ()=>{
     onReg === false;
@@ -43,62 +43,106 @@ let reg_off = ()=>{
 
     onLogIn = false;
     modailLogIn.style = "display:none";
-}
+
+    passRegI.value = ""
+    passCanfRegI.value = ""
+    nameRegI.value = ""
+};
 
 let logIn_off = ()=>{
     onLogIn = false;
     modailLogIn.style = "display:none";
-}
+
+    logInName.value=""
+    logInPass.value=""
+};
 
 document.querySelector(".log_in_button").addEventListener("click", () => {
     modail_off_on();
-})
+});
 
 regButton.addEventListener("click", ()=>{
     reg_on();
-})
+});
 
 modailLogIn_off.addEventListener("click",()=>{
     logIn_off();
-    logInName.value=""
-    logInPass.value=""
-})
+    
+});
 
 regOff.addEventListener("click",()=>{
     reg_off()
-})
+});
 
 document.getElementById("reg_buttono_cansel").addEventListener('click',()=>{
     reg_off()
-    passRegI.value = ""
-    passCanfRegI.value = ""
-    nameRegI.value = ""
-
-})
+});
 
 //=================== <> =============================
+let locStorage = window.localStorage
+let reginfo = {};
+let localKay = locStorage.getItem("key");
+
+let locKayGen = () =>{
+    console.log(localKay)
+    if(localKay === null){
+        locStorage.setItem("key","0")
+    }else{
+        locStorage.setItem("key",localKay++)
+    }
+}
+
+let saveAccount = ()=>{
+
+    let jsonRegInfo = JSON.stringify(reginfo)
+    locStorage.setItem(localKay,jsonRegInfo)
+    locKayGen()
+    
+};
+//=================== <> =============================
+let error = document.querySelector(".log_error");
+let errorOff = document.getElementById("log_error__off");
+let errorLog = document.getElementById("error__inf");
 
 let regConf = document.getElementById("reg_start");
-let reginfo = {};
+
 let regError;
 
+let error_act = (f)=>{
+    errorLog.innerText = f;
+}
+
+errorOff.addEventListener("click", ()=>{
+    error.style = "display:none"
+})
+
+
 let registration = () => {
+
     if(nameRegI.value.length >=3){
         reginfo.name = nameRegI.value;
-        if(passRegI.value === passCanfRegI.value){
-            if(passRegI.value.length >= 6){
-                reginfo.pass = passRegI.value
+        if(passRegI.value.length != 0){
+            if(passRegI.value === passCanfRegI.value){
+                if(passRegI.value.length >= 6){
+                    reginfo.pass = passRegI.value
+                    error.style = "display:none"
+                    reg_off()
+                    saveAccount()
+                }else{
+                    error_act("The password is too short")
+                    error.style = "display:flex"
+                }
             }else{
-                regError = "Короткий Пороль"
-                alert(regError)
-            }
+                error_act("Password confirmation does not match")
+                error.style = "display:flex"
+            }    
         }else{
-            regError = "Подтверждение пароля не совподает"
-            alert(regError)
-        }    
+            error_act("Enter password")
+            error.style = "display:flex"
+        }
     }else{
-        regError = "Имя слишком короткое"
-        alert(regError)
+        error_act("Name is too short")
+        error.style = "display:flex"
     }
     
 }
@@ -107,3 +151,5 @@ regConf.addEventListener("click", ()=>{
     registration()
     console.log(reginfo)
 })
+
+
